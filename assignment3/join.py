171891b5ba5b3f6dd48fe8de84f12fb60959f939
Junxiong Huang <jhuang292@wisc.edu>
2019-02-25 22:38:52 -0600

    mapreduce join

--- assignment3/join.py
+++ assignment3/join.py
@@ -11,12 +11,27 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+    key = record[1]
+    value = record
+    mr.emit_intermediate(key, value)
+    
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, list_of_values):
+    order_of_list, line_item_of_list = [], []
+    for v in list_of_values:
+        if len(v) == 10:
+            order_of_list.append(v)
+        else:
+            line_item_of_list.append(v)
+
+    for item1 in order_of_list:
+        for item2 in line_item_of_list:
+            mr.emit((item1 + item2))
+
+
+    
 
 # Do not modify below this line
 # =============================

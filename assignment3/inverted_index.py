2164096e85f54478e58eaed1d9d420105e29a097
jhuang292 <jhuang292@wisc.edu>
2019-02-25 19:56:13 -0600

    inverted_index.py

--- assignment3/inverted_index.py
+++ assignment3/inverted_index.py
@@ -11,12 +11,17 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(doc_list):
+    document_id = doc_list[0]
+    text = doc_list[1]
+    tokens = text.split()
+    for token in tokens:
+        mr.emit_intermediate(token, document_id)
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, value):
+    mr.emit([key,list(set(value))])
+    
 
 # Do not modify below this line
 # =============================

3ef33a8ea26cd139810fbd012894ec8050378954
jhuang292 <jhuang292@wisc.edu>
2019-02-26 21:26:19 -0600

    mapReduce for matrices multiplication

--- assignment3/multiply.py
+++ assignment3/multiply.py
@@ -11,12 +11,29 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+    if record[0] == 'a':
+        key = record[1]
+        value = record
+        mr.emit_intermediate(key, value)
+    else:
+        mr.emit_intermediate(0, record)
+        mr.emit_intermediate(1, record)
+        mr.emit_intermediate(2, record)
+        mr.emit_intermediate(3, record)
+        mr.emit_intermediate(4, record)
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, list_of_values):
+    a_list = list_of_values[:5]
+    b_list = list_of_values[5:]
+    total = 0
+    for i in range(5):
+        for j in range(5):
+            total += a_list[j][-1] * b_list[5*j+i][-1]
+        mr.emit((key, i, total))
+        total = 0
+
 
 # Do not modify below this line
 # =============================

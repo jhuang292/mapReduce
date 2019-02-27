2cdd13a4dabe340862d767316c88ed208392f28e
jhuang292 <jhuang292@wisc.edu>
2019-02-26 19:05:00 -0600

    mapReduce check whether symmetric friendship holds

--- assignment3/assymmentric_friendships.py
+++ assignment3/assymmentric_friendships.py
@@ -11,12 +11,17 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+    sorted_list = sorted(record)
+    key = str(sorted_list[0]) + " " + str(sorted_list[1])
+    value = record
+    mr.emit_intermediate(key, value)
+    
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, list_of_values):
+    if len(list_of_values) == 1:
+        mr.emit((list_of_values[0][1], list_of_values[0][0]))
 
 # Do not modify below this line
 # =============================
